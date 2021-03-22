openwrt/ubox

commit 9ef886819dd48303d8ced4cdbc9afbf32682535c

logd: self-degrade to 'logd' user after opening pipes
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

log/logd.c
@@ -11,6 +11,8 @@
  * GNU General Public License for more details.
  */
 
+#include <sys/types.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <syslog.h>
@@ -238,6 +240,7 @@ int
 main(int argc, char **argv)
 {
 	int ch, log_size = 16;
+	struct passwd *p = NULL;
 
 	signal(SIGPIPE, SIG_IGN);
 	while ((ch = getopt(argc, argv, "S:")) != -1) {
@@ -255,6 +258,11 @@ main(int argc, char **argv)
 	log_init(log_size);
 	conn.cb = ubus_connect_handler;
 	ubus_auto_connect(&conn);
+	p = getpwnam("logd");
+	if (p) {
+		setuid(p->pw_uid);
+		setgid(p->pw_gid);
+	}
 	uloop_run();
 	log_shutdown();
 	uloop_done();

commit 41664054b8b1bc19d842421c49d1b4d612ff8297

logd: fix ignored return values in set{gid,uid}
Ubuntu 18.05 with gcc-7.5 yields following error:

 ubox/log/logd.c:263:3: error: ignoring return value of ‘setuid’, declared with attribute warn_unused_result [-Werror=unused-result]
   setuid(p->pw_uid);
   ^~~~~~~~~~~~~~~~~
 ubox/log/logd.c:264:3: error: ignoring return value of ‘setgid’, declared with attribute warn_unused_result [-Werror=unused-result]
   setgid(p->pw_gid);
   ^~~~~~~~~~~~~~~~~

Fixes: 9ef886819dd4 ("logd: self-degrade to 'logd' user after opening pipes")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

log/logd.c
@@ -260,8 +260,15 @@ main(int argc, char **argv)
 	ubus_auto_connect(&conn);
 	p = getpwnam("logd");
 	if (p) {
-		setuid(p->pw_uid);
-		setgid(p->pw_gid);
+		if (setuid(p->pw_uid) < 0) {
+			fprintf(stderr, "setuid() failed: %s\n", strerror(errno));
+			exit(1);
+		}
+
+		if (setgid(p->pw_gid) < 0) {
+			fprintf(stderr, "setgid() failed: %s\n", strerror(errno));
+			exit(1);
+		}
 	}
 	uloop_run();
 	log_shutdown();