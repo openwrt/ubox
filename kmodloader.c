openwrt/ubox

commit 4c7b720b9c63b826fb9404e454ae54f2ef5649d5

kmodloader: fix GCC fanalyzer warnings
memory leaks and missing NULL checks.

Signed-off-by: Rosen Penev <rosenp@gmail.com>

kmodloader.c
@@ -336,6 +336,11 @@ static int scan_loaded_modules(void)
 			/* possibly a module outside /lib/modules/<ver>/ */
 			n = alloc_module(m.name, NULL, 0, m.depends, m.size);
 		}
+		if (!n) {
+			ULOG_ERR("Failed to allocate memory for module\n");
+			return -1;
+		}
+
 		n->usage = m.usage;
 		n->state = LOADED;
 	}
@@ -582,6 +587,11 @@ static int insert_module(char *path, const char *options)
 	void *data = 0;
 	struct stat s;
 	int fd, ret = -1;
+
+	if (!path) {
+		ULOG_ERR("Path not specified\n");
+		return ret;
+	}
 
 	if (stat(path, &s)) {
 		ULOG_ERR("missing module %s\n", path);
@@ -1164,6 +1174,8 @@ load_options(void)
 			continue;
 		}
 	}
+
+	fclose(f);
 }
 
 int main(int argc, char **argv)

commit 811ca6c2234a3d13efef55947a9cff8bef56ceb7

kmodloader: fix memory leak in scan_loaded_modules()
Commit 4c7b720b9c63 fixed some error-handling paths but introduced another
memory leak; update and fix.

Fixes: 4c7b720b9c63 ("kmodloader: fix GCC fanalyzer warnings")
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

kmodloader.c
@@ -311,12 +311,13 @@ static int scan_loaded_modules(void)
 {
 	size_t buf_len = 0;
 	char *buf = NULL;
+	int rv = -1;
 	FILE *fp;
 
 	fp = fopen("/proc/modules", "r");
 	if (!fp) {
 		ULOG_ERR("failed to open /proc/modules\n");
-		return -1;
+		goto out;
 	}
 
 	while (getline(&buf, &buf_len, fp) > 0) {
@@ -338,16 +339,18 @@ static int scan_loaded_modules(void)
 		}
 		if (!n) {
 			ULOG_ERR("Failed to allocate memory for module\n");
-			return -1;
+			goto out;
 		}
 
 		n->usage = m.usage;
 		n->state = LOADED;
 	}
+	rv = 0;
+out:
 	free(buf);
 	fclose(fp);
 
-	return 0;
+	return rv;
 }
 
 static struct module* get_module_info(const char *module, const char *name)