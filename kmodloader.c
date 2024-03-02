openwrt/ubox

commit c105f22064d6f2d5cb55282d0439d924e827e5ac

kmodloader: eliminate some hardcoded buffer sizes
Signed-off-by: Felix Fietkau <nbd@openwrt.org>

kmodloader.c
@@ -191,15 +191,17 @@ alloc_module(const char *name, const char *depends, int size)
 
 static int scan_loaded_modules(void)
 {
-	FILE *fp = fopen("/proc/modules", "r");
-	char buf[256];
+	size_t buf_len = 0;
+	char *buf = NULL;
+	FILE *fp;
 
+	fp = fopen("/proc/modules", "r");
 	if (!fp) {
 		fprintf(stderr, "failed to open /proc/modules\n");
 		return -1;
 	}
 
-	while (fgets(buf, sizeof(buf), fp)) {
+	while (getline(&buf, &buf_len, fp) > 0) {
 		struct module m;
 		struct module *n;
 
@@ -215,6 +217,8 @@ static int scan_loaded_modules(void)
 		n->usage = m.usage;
 		n->state = LOADED;
 	}
+	free(buf);
+	fclose(fp);
 
 	return 0;
 }
@@ -452,9 +456,8 @@ static int print_usage(char *arg)
 
 static int main_insmod(int argc, char **argv)
 {
-	char options[256] = "";
-	char *name;
-	int i;
+	char *name, *cur, *options;
+	int i, ret, len;
 
 	if (argc < 2)
 		return print_insmod_usage();
@@ -476,13 +479,24 @@ static int main_insmod(int argc, char **argv)
 
 	free_modules();
 
-	for (i = 2; i < argc; i++)
-		if (snprintf(options, sizeof(options), "%s %s", options, argv[i]) >= sizeof(options)) {
-			fprintf(stderr, "argument line too long - %s\n", options);
-			return -1;
-		}
+	for (len = 0, i = 2; i < argc; i++)
+		len += strlen(argv[i]) + 1;
 
-	return insert_module(get_module_path(name), options);
+	options = malloc(len);
+	options[0] = 0;
+	cur = options;
+	for (i = 2; i < argc; i++) {
+		if (options[0]) {
+			*cur = ' ';
+			cur++;
+		}
+		cur += sprintf(cur, "%s", argv[i]);
+	}
+
+	ret = insert_module(get_module_path(name), options);
+	free(options);
+
+	return ret;
 }
 
 static int main_rmmod(int argc, char **argv)
@@ -553,7 +567,7 @@ static int main_depmod(int argc, char **argv)
 {
 	struct utsname ver;
 	struct module *m;
-	char path[128];
+	char *path;
 	char *name;
 
 	if (argc != 2)
@@ -563,6 +577,7 @@ static int main_depmod(int argc, char **argv)
 		return -1;
 
 	uname(&ver);
+	path = alloca(sizeof(DEF_MOD_PATH "*.ko") + strlen(ver.release) + 1);
 	snprintf(path, sizeof(path), DEF_MOD_PATH "*.ko", ver.release);
 
 	scan_module_folder(path);
@@ -608,14 +623,15 @@ static int main_loader(int argc, char **argv)
 
 	for (j = 0; j < gl.gl_pathc; j++) {
 		FILE *fp = fopen(gl.gl_pathv[j], "r");
-		char mod[256];
+		size_t mod_len = 0;
+		char *mod = NULL;
 
 		if (!fp) {
 			fprintf(stderr, "failed to open %s\n", gl.gl_pathv[j]);
 			continue;
 		}
 
-		while (fgets(mod, sizeof(mod), fp)) {
+		while (getline(&mod, &mod_len, fp) > 0) {
 			char *nl = strchr(mod, '\n');
 			struct module *m;
 			char *opts;
@@ -632,6 +648,7 @@ static int main_loader(int argc, char **argv)
 				continue;
 			insert_module(get_module_path(mod), (opts) ? (opts) : (""));
 		}
+		free(mod);
 		fclose(fp);
 	}
 

commit 4c5c45c6beac586c994af34a795bc5f2232f5f3d

kmodloader: fix invalid write during insmod, CodeQL warnings
Running 'insmod <module>' (without module options) tries to write an empty
option string (1 byte) to a mis-allocated zero-size memory block:

  ==381835== Command: ./insmod unix
  ==381835==
  ==381835== Invalid write of size 1
  ==381835==    at 0x10A874: main_insmod (kmodloader.c:944)
  ==381835==    by 0x10A874: main (kmodloader.c:1383)
  ==381835==  Address 0x4ab6f60 is 0 bytes after a block of size 0 alloc'd
  ==381835==    at 0x4848899: malloc (in vgpreload_memcheck-amd64-linux.so)
  ==381835==    by 0x10A856: main_insmod (kmodloader.c:937)
  ==381835==    by 0x10A856: main (kmodloader.c:1383)

Change main_insmod() to allocate at least 1 byte in this case for the
null-termination. Also rename local 'options' -> 'opts' to avoid confusion
with similar global, as suggested by CodeQL, and clarify pathname logic.

Fixes: c105f22064d6 ("kmodloader: eliminate some hardcoded buffer sizes")
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

kmodloader.c
@@ -892,7 +892,7 @@ static int print_usage(char *arg)
 
 static int main_insmod(int argc, char **argv)
 {
-	char *name, *cur, *options = NULL;
+	char *name, *path, *cur, *opts = NULL;
 	int i, ret = -1, len;
 
 	if (argc < 2)
@@ -913,19 +913,19 @@ static int main_insmod(int argc, char **argv)
 
 	}
 
-	for (len = 0, i = 2; i < argc; i++)
+	for (len = 1, i = 2; i < argc; i++)
 		len += strlen(argv[i]) + 1;
 
-	options = malloc(len);
-	if (!options) {
+	opts = malloc(len);
+	if (!opts) {
 		ULOG_ERR("out of memory\n");
 		goto err;
 	}
 
-	options[0] = 0;
-	cur = options;
+	opts[0] = 0;
+	cur = opts;
 	for (i = 2; i < argc; i++) {
-		if (options[0]) {
+		if (opts[0]) {
 			*cur = ' ';
 			cur++;
 		}
@@ -937,19 +937,18 @@ static int main_insmod(int argc, char **argv)
 		goto err;
 	}
 
-	if (get_module_path(argv[1])) {
-		name = argv[1];
-	} else if (!get_module_path(name)) {
+	if (!(path = get_module_path(argv[1])) ||
+	     (path = get_module_path(name))) {
 		fprintf(stderr, "Failed to find %s. Maybe it is a built in module ?\n", name);
 		goto err;
 	}
 
-	ret = insert_module(get_module_path(name), options);
+	ret = insert_module(path, opts);
 	if (ret)
 		ULOG_ERR("failed to insert %s\n", get_module_path(name));
 
 err:
-	free(options);
+	free(opts);
 	free_modules();
 	free_module_folders();
 